0d349ac5221f6ed32835a8d022bdbb64047d8820
adeliktas <adeliktas@web.de>
2022-01-03 22:15:03 +0100

    added allemeineentchen and nyancat

---
+++ allemeineentchen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#original creator Speedy_92 C++ https://www.c-plusplus.net/forum/topic/219056/alle-meine-entchen
+#translated to beep by adeliktas
+beep -f 262 -l 500 -n -f 294 -l 500 -n -f 330 -l 500 -n -f 349 -l 500 -n -f 392 -l 1000 -n -f 392 -l 1000 -n -f 440 -l 500 -n -f 440 -l 500 -n -f 440 -l 500 -n -f 440 -l 500 -n -f 392 -l 1500 -n -f 440 -l 500 -n -f 440 -l 500 -n -f 440 -l 500 -n -f 440 -l 500 -n -f 392 -l 1500 -n -f 349 -l 500 -n -f 349 -l 500 -n -f 349 -l 500 -n -f 349 -l 500 -n -f 330 -l 1000 -n -f 330 -l 1000 -n -f 294 -l 500 -n -f 294 -l 500 -n -f 294 -l 500 -n -f 294 -l 500 -n -f 262 -l 1500
